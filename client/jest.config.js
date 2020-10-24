968622d3db495cd6b62841f4a5e6fc04241e2ac3
spacedawn96 <spacedawn96@gmail.com>
2020-10-24 15:30:21 +0900

    client init

--- client/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  coverageDirectory: 'coverage',
-  moduleNameMapper: {
-    '\\.(css|less|scss)$': 'identity-obj-proxy'
-  },
-  // set up for enzyme and allowing scss
-  setupFilesAfterEnv: ['<rootDir>/ui/tests/_setup.tests.ts']
-};
